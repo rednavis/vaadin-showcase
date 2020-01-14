06df369afc5335c37b1c4a5ed2094a3affc74d13
Kate Novik <ekat.novik@gmail.com>
2020-01-15 01:30:52 +0300

    Fix code style changes.

--- frontend/src/views/login/login-view.js
+++ frontend/src/views/login/login-view.js
@@ -1,6 +1,6 @@
 import {PolymerElement} from "@polymer/polymer/polymer-element.js";
-import '@polymer/iron-iconset-svg/iron-iconset-svg.js';
-import '@polymer/iron-icon/iron-icon.js';
+import "@polymer/iron-iconset-svg/iron-iconset-svg.js";
+import "@polymer/iron-icon/iron-icon.js";
 import {html} from "@polymer/polymer/lib/utils/html-tag.js";
 
 class LoginView extends PolymerElement {
