c0fff37bbb674f9d2597664ca8a1274c2c87941b
Kate Novik <ekat.novik@gmail.com>
2020-01-17 17:48:57 +0300

    Create appropriate log in/sign up form, add styles.

--- frontend/src/views/login/login-view.js
+++ frontend/src/views/login/login-view.js
@@ -1,11 +1,238 @@
 import {PolymerElement} from "@polymer/polymer/polymer-element.js";
 import "@polymer/iron-iconset-svg/iron-iconset-svg.js";
 import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/iron-form/iron-form.js";
+import "@polymer/paper-tabs/paper-tabs.js";
+import "@polymer/paper-tabs/paper-tab.js";
+import "@polymer/iron-pages/iron-pages.js";
+import "@vaadin/vaadin-button/vaadin-button";
+import "@vaadin/vaadin-text-field/vaadin-text-field";
+import "@vaadin/vaadin-text-field/vaadin-password-field";
+import "@vaadin/vaadin-ordered-layout/vaadin-vertical-layout";
 import {html} from "@polymer/polymer/lib/utils/html-tag.js";
 
 class LoginView extends PolymerElement {
+
+static get properties() {
+
+  return {
+    selected: {
+      type: Number,
+      value: 0,
+      notify: true,
+      reflectToAttribute: true
+    }
+  }
+}
+
+
   static get template() {
     return html`
+      <style>
+        :host {
+          --color-facebook: #3e5b96;
+          --color-twitter: #2aa3ef;
+          --color-google: #db4e47;
+          display: flex;
+          width: 400px;
+          margin: 0 auto;
+          flex-direction: column;
+          padding-top: 300px;
+          --lumo-border-radius-m: 0.25em;
+          --lumo-font-size-s: .875rem;
+          --lumo-secondary-text-color: hsla(214, 42%, 18%, 0.72);
+          --lumo-primary-text-color: hsl(214, 90%, 52%); 
+        }
+        
+        iron-form {
+          box-shadow: 0 0 10px rgba(0,0,0,0.5);
+          padding: 15px;
+          background: lightslategray;
+        }
+        
+        .login-form-container .text-upper {
+          text-transform: uppercase;
+          text-align: center;
+          overflow: hidden;
+        }
+        
+        .social-button iron-icon {
+          position: absolute;
+          left: .5rem;
+          top: .44rem;
+       }
+       
+       iron-icon {
+          display: -ms-inline-flexbox;
+          display: -webkit-inline-flex;
+          display: inline-flex;
+          -ms-flex-align: center;
+          -webkit-align-items: center;
+          align-items: center;
+          -ms-flex-pack: center;
+          -webkit-justify-content: center;
+          justify-content: center;
+       }
+       .registration-dialog__social-button {
+          position: relative;
+          width: 100%;
+          margin-top: .3rem;
+       }
+      .social-button {
+          font-family: 'Montserrat';
+          text-transform: none;
+          border-radius: 50px;
+          font-weight: 500;
+          margin-bottom: .5rem;
+          padding: .4rem .5rem;
+          display: inline-block;
+          text-align: center;
+      }
+      .button--social-text {
+         font-weight: 700;
+         font-size: 15px;
+      }
+      button, input, optgroup, select, textarea {
+        color: inherit;
+        font: inherit;
+        margin: 0;
+      }
+      .button {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        background: transparent;
+        font-family: inherit;
+        font-size: 1em;
+        cursor: pointer;
+        outline: 0;
+        font-weight: 700;
+        line-height: 1.6;
+        color: #36393d;
+        font-size: 0.875rem;
+        color: #00b4f0;
+        margin-bottom: 8px;
+        position: relative;
+        display: inline-block;
+        height: auto;
+        text-decoration: none;
+        margin-top: 1.5rem;
+        font-weight: 700;
+        text-decoration: none;
+        text-transform: uppercase;
+        letter-spacing: .1em;
+        transition: 100ms;
+      }
+      .button--bordered, .button--filled, .button--white {
+        font-family: 'Montserrat';
+        margin-bottom: 0;
+        margin: 0;
+        border: 0;
+        padding: .8rem 1.6rem;
+        background: #00b4f0;
+        border-radius: 1000px;
+        color: #fff;
+        font-weight: 700;
+        text-transform: uppercase;
+        margin-top: 1rem;
+      }
+      .login-form-container .button--twitter {
+        background-color: var(--color-twitter);
+        color: #fff;
+      } 
+      .login-form-container .button--google {
+        background-color: var(--color-google);
+        color: #fff;
+      }
+      .login-form-container .button--facebook {
+        background-color: var(--color-facebook);
+        color: #fff;
+      }
+
+      
+      vaadin-text-field, vaadin-password-field {
+        width: 100%;
+      }
+      [part="label"]::after {
+        content: "•";
+        transition: opacity 0.2s;
+        opacity: 0;
+        color: var(--lumo-primary-text-color);
+        position: absolute;
+        right: 0;
+        width: 1em;
+        text-align: center;
+      }
+      [part="label"] {
+        align-self: flex-start;
+        color: var(--lumo-secondary-text-color);
+        font-weight: 500;
+        font-size: var(--lumo-font-size-s);
+        margin-left: calc(var(--lumo-border-radius-m) / 4);
+        transition: color 0.2s;
+        line-height: 1;
+        padding-bottom: 0.5em;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        position: relative;
+        max-width: 100%;
+        box-sizing: border-box;
+      }
+
+      label {
+        cursor: default;
+      }
+      .login-form-container .vc-input {
+        width: 100%;
+      }
+
+      .plain-tabs {
+        --paper-tabs-selection-bar-color: #34C65D;
+        --paper-tabs: {
+        font-family: 'Montserrat';
+        color: #fff;
+        font-size: 16px;
+        font-weight: 700;
+        /*--lumo-contrast-60pct: #aaa;*/
+        /*--lumo-body-text-color: #fff;*/
+        display: inline-block;
+        max-width: 100%;
+        width: auto 
+        }
+      }
+
+      .or-with-lines {
+        text-align: center;
+        overflow: hidden;
+      }
+      .or-with-lines > span:before {
+        right: 100%;
+        margin-right: 20px;
+      }
+      
+      .or-with-lines > span:after {
+        left: 100%;
+        margin-left: 20px;
+      }
+
+      .or-with-lines > span:before, 
+      .or-with-lines > span:after {
+        content: "";
+        position: absolute;
+        height: 1px;
+        top: 50%;
+        background-color: #fff;
+        width: 200px;
+      }
+      .or-with-lines > span {
+        display: inline-block;
+        position: relative;
+        font-size: 14px;
+        font-weight: 700;
+      }
+      
+      </style>
       <iron-iconset-svg name="inline" size="24">
         <svg>
           <defs>
@@ -26,19 +253,139 @@ class LoginView extends PolymerElement {
         </svg>
       </iron-iconset-svg>
 
-      <div id="header">Vaadin Showcase</div>
-      <div id="content">Log In</div>
-      <hr>
-      <iron-icon icon="inline:google" id="google-icon" role="img" aria-label="Google"></iron-icon>
-      <iron-icon icon="inline:facebook" id="facebook-icon" role="img" aria-label="Facebook"></iron-icon>
-      <iron-icon icon="inline:twitter" id="twitter-icon" role="img" aria-label="Twitter"></iron-icon>
-      <hr>
-      <div id="footer">(c)RedNavis</div>`;
+      <div class="dialog-header">
+      <s-svg src="img/rednavis-logo.svg" />
+      </div>
+      
+      <paper-tabs id="plain-tabs" class="plain-tabs" selected="{{selected}}">
+        <paper-tab>Login</paper-tab>
+        <paper-tab>Sign up</paper-tab>
+      </paper-tabs>
+
+
+<!--      <div class="content">-->
+      <iron-pages selected="{{selected}}">
+       <div>
+             <iron-form class="login" id="form" allow-redirect>
+
+      <div class="login-form-container">
+                  <button class="registration-dialog__social-button social-button button--google">
+         <iron-icon icon="inline:google" id="google-icon" class="button-icon" role="img" aria-label="Google"></iron-icon>
+         <span class="button--social-text">Login with Google</span>
+      </button>
+      <button class="registration-dialog__social-button social-button button--facebook">
+         <iron-icon icon="inline:facebook" id="facebook-icon" class="button-icon" role="img" aria-label="Facebook"></iron-icon>
+         <span class="button--social-text">Login with Facebook</span>
+      </button>
+      <button class="registration-dialog__social-button social-button button--twitter">
+         <iron-icon icon="inline:twitter" id="twitter-icon" class="button-icon" role="img" aria-label="Twitter"></iron-icon>
+         <span class="button--social-text">Login with Twitter</span>
+      </button>
+      
+      <p class="text-upper text-muted or-with-lines"><span>Or</span></p>
+                <form method="post" action="login">
+                    <vaadin-vertical-layout>
+                        <vaadin-text-field id="username" name="email" autocomplete="email" class="vc-input" label="Username" autofocus required has-label tabindex="0"></vaadin-text-field>
+                        <input type="text" part="value" slot="input" name="email" tabindex="0" aria-labelledby="vaadin-text-field-label-3 vaadin-text-field-input-3" required="" aria-required="" autocomplete="email">
+                        <vaadin-password-field autocomplete="current-password" id="password" class="vc-input" type="password" label="Password" name="password" required="" has-label="" tabindex="0">
+                        <input type="password" part="value" slot="input" name="password" tabindex="0" aria-labelledby="vaadin-password-field-label-4 vaadin-password-field-input-4" required="" aria-required="" autocomplete="current-password"></vaadin-password-field>
+                        <vaadin-button class="button button--filled registration-dialog__button" on-click="login">
+                            LOGIN
+                        </vaadin-button>
+                    </vaadin-vertical-layout>
+                </form>
+      </div>
+                  </iron-form>
+       </div>
+       <div>
+              <iron-form class="login" id="form" allow-redirect>
+
+      <div class="login-form-container">
+                  <button class="registration-dialog__social-button social-button button--google">
+         <iron-icon icon="inline:google" id="google-icon" class="button-icon" role="img" aria-label="Google"></iron-icon>
+         <span class="button--social-text">Sign up with Google</span>
+      </button>
+      <button class="registration-dialog__social-button social-button button--facebook">
+         <iron-icon icon="inline:facebook" id="facebook-icon" class="button-icon" role="img" aria-label="Facebook"></iron-icon>
+         <span class="button--social-text">Sign up with Facebook</span>
+      </button>
+      <button class="registration-dialog__social-button social-button button--twitter">
+         <iron-icon icon="inline:twitter" id="twitter-icon" class="button-icon" role="img" aria-label="Twitter"></iron-icon>
+         <span class="button--social-text">Sign up with Twitter</span>
+      </button>
+      
+      <p class="text-upper text-muted or-with-lines"><span>Or</span></p>
+<!--                <form method="post" action="signup">-->
+<!--                    <vaadin-vertical-layout>-->
+<!--                        <vaadin-text-field id="email" name="email" placeholder="Email" autofocus required></vaadin-text-field>-->
+<!--                        <vaadin-password-field id="password" name="password" placeholder="Password" required></vaadin-password-field>-->
+<!--                        <vaadin-button on-click="signup" theme="primary">-->
+<!--                            SIGN UP-->
+<!--                        </vaadin-button>-->
+<!--                    </vaadin-vertical-layout>-->
+<!--                </form>-->
+                <form id="provideEmailForm">
+                <vaadin-vertical-layout>
+                <vaadin-text-field autocomplete="email" id="signup-email" class="vc-input" label="Email" error-message="Invalid Email Format" required="" name="email" tabindex="0" has-label="">
+                  <input type="email" part="value" slot="input" name="email" tabindex="0" aria-labelledby="vaadin-text-field-label-5 vaadin-text-field-input-5" required="" aria-required="" pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z0-9-]{2,63}$" autocomplete="email">
+                </vaadin-text-field>
+                <vaadin-password-field autocomplete="new-password" id="signup-password" class="vc-input" type="password" label="Password" minlength="8" error-message="Password must be at least 8 characters" name="password" required="" tabindex="0" has-label="">
+                  <input type="password" part="value" slot="input" name="password" tabindex="0" aria-labelledby="vaadin-password-field-label-6 vaadin-password-field-input-6" required="" aria-required="" minlength="8" autocomplete="new-password">
+                </vaadin-password-field>
+                <button type="submit" class="button button--filled button--create-account">Sign up</button>
+                </vaadin-vertical-layout>
+                </form>
+      </div>
+                  </iron-form>
+       </div>
+      </iron-pages>
+      </div>
+      
+<!--      <iron-form class="login" id="form" allow-redirect>-->
+
+<!--      <div id="header">Vaadin Showcase</div>-->
+<!--      <div id="content">Log In</div>-->
+
+<!--      <div class="login-form-container">-->
+<!--                  <button class="registration-dialog__social-button social-button button&#45;&#45;google">-->
+<!--         <iron-icon icon="inline:google" id="google-icon" class="button-icon" role="img" aria-label="Google"></iron-icon>-->
+<!--         <span class="button&#45;&#45;social-text">Login with Google</span>-->
+<!--      </button>-->
+<!--      <button class="registration-dialog__social-button social-button button&#45;&#45;facebook">-->
+<!--         <iron-icon icon="inline:facebook" id="facebook-icon" class="button-icon" role="img" aria-label="Facebook"></iron-icon>-->
+<!--         <span class="button&#45;&#45;social-text">Login with Facebook</span>-->
+<!--      </button>-->
+<!--      <button class="registration-dialog__social-button social-button button&#45;&#45;twitter">-->
+<!--         <iron-icon icon="inline:twitter" id="twitter-icon" class="button-icon" role="img" aria-label="Twitter"></iron-icon>-->
+<!--         <span class="button&#45;&#45;social-text">Login with Twitter</span>-->
+<!--      </button>-->
+<!--      -->
+<!--      <p class="text-upper text-muted or-with-lines"><span>Or</span></p>-->
+<!--                <form method="post" action="login">-->
+<!--                    <vaadin-vertical-layout>-->
+<!--                        <vaadin-text-field id="username" name="username" placeholder="Username" autofocus required></vaadin-text-field>-->
+<!--                        <vaadin-password-field id="password" name="password" placeholder="Password" required></vaadin-password-field>-->
+<!--                        <vaadin-button on-click="login" theme="primary">-->
+<!--                            Login-->
+<!--                        </vaadin-button>-->
+<!--                    </vaadin-vertical-layout>-->
+<!--                </form>-->
+<!--      </div>-->
+<!--                  </iron-form>-->
+
+<!-- -->    `;
   }
 
   static get is() {
     return "login-view";
   }
+
+  login() {
+    if (!this.$.username.invalid && !this.$.password.invalid) {
+      this.$.form.submit();
+    }
+  }
+
 }
 
 customElements.define(LoginView.is, LoginView);
--- src/main/java/com/rednavis/vaadin/showcase/views/login/LoginView.java
+++ src/main/java/com/rednavis/vaadin/showcase/views/login/LoginView.java
@@ -4,27 +4,10 @@ import static com.rednavis.vaadin.showcase.views.ConstantUtils.PAGE_LOGIN_TITLE;
 import static com.rednavis.vaadin.showcase.views.ConstantUtils.PAGE_LOGIN_URL;
 import static com.rednavis.vaadin.showcase.views.ConstantUtils.VIEW_PORT;
 
-import com.rednavis.vaadin.showcase.temp.Error401Temp;
-import com.rednavis.vaadin.showcase.temp.Error403Temp;
-import com.rednavis.vaadin.showcase.temp.Error500Temp;
-import com.rednavis.vaadin.showcase.views.ConstantUtils;
-import com.rednavis.vaadin.showcase.views.dashboard.DashboardView;
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -38,65 +21,19 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @RouteAlias(value = "")
 @Tag("login-view")
 @JsModule("./src/views/login/login-view.js")
+//@NpmPackage(value="@polymer/iron-form", version = "3.0.1")
+//@NpmPackage(value="@polymer/paper-tabs", version = "3.1.0")
+//@NpmPackage(value="@polymer/iron-pages", version = "3.0.1")
 @Viewport(VIEW_PORT)
 public class LoginView extends PolymerTemplate<TemplateModel> {
 
-  @Id("content")
-  private Div content;
-
-  private TextField userName;
-  private PasswordField password;
-  private Label userNameLabel;
-  private Label passwordLabel;
-  private Checkbox rememberMe;
-  private Button loginButton;
+//  @Id("content")
+//  private Div content;
 
   /**
    * LoginView.
    */
   public LoginView() {
-    createForm();
-    Button goDashboard = new Button("Go Dashboard");
-    goDashboard.addClickListener(
-        (ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> UI.getCurrent().navigate(DashboardView.class));
 
-    Button error401 = new Button("Error 401");
-    error401.addClickListener((ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> UI.getCurrent().navigate(Error401Temp.class));
-
-    Button error403 = new Button("Error 403");
-    error403.addClickListener((ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> UI.getCurrent().navigate(Error403Temp.class));
-
-    Button error404 = new Button("Error 404");
-    error404.addClickListener((ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> UI.getCurrent().navigate("wrongUrl"));
-
-    Button error500 = new Button("Error 500");
-    error500.addClickListener((ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> UI.getCurrent().navigate(Error500Temp.class));
-
-    VerticalLayout verticalLayout = new VerticalLayout(goDashboard, error401, error403, error404, error500);
-    content.add(verticalLayout);
-  }
-
-  private void createForm() {
-    userName = new TextField();
-    password = new PasswordField();
-    loginButton = new Button();
-    userNameLabel = new Label();
-    passwordLabel = new Label();
-    rememberMe = new Checkbox();
-    Element userNameInput = new Element(Tag.INPUT);
-    Element passwordInput = new Element(Tag.INPUT);
-    userNameInput.setAttribute("slot", "input");
-    userNameInput.setAttribute("name", "username");
-    passwordInput.setAttribute("slot", "input");
-    passwordInput.setAttribute("name", "password");
-    userName.getElement().appendChild(userNameInput);
-    password.getElement().appendChild(passwordInput);
-    loginButton.setId("login");
-    userNameLabel.setText(ConstantUtils.LOGIN_FORM_LOGIN);
-    passwordLabel.setText(ConstantUtils.LOGIN_FORM_PASSWORD);
-    userName.setPlaceholder(ConstantUtils.LOGIN_FORM_TYPE_A_LOGIN);
-    password.setPlaceholder(ConstantUtils.LOGIN_FORM_TYPE_A_PASSWORD);
-    rememberMe.setLabel(ConstantUtils.LOGIN_FORM_REMEMBER_ME);
-    loginButton.setText(ConstantUtils.BUTTON_LOGIN);
   }
 }
